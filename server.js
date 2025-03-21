ca41f3765efcefe41945892e5254b6c931b170ea
Tahnil <genosse.stanil@me.com>
2025-03-21 22:15:56 +0100

    setup for running behind a proxy

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Setup for running behind a proxy
+app.set('trust proxy', true);
+
 // Enable CORS for Google Sheets
 app.use(cors());
 
